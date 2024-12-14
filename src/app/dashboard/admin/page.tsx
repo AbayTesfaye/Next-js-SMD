de454a084f8f478e1301ae4f610790c7151b4734
Abay Tesfaye <abaytesfaye02@gmail.com>
2024-12-14 17:53:28 +0300

    Done for Finance chart

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -21,6 +22,7 @@ const AdminPage = () => {
           <div className="w-full lg:w-2/3 h-[450px] "><AttendanceChart/></div>
         </div>
         {/* bottom chart */}
+        <div className="w-full h-[500px]"><FinanceChart/></div>
       </div>
        {/* RIGHT */}
        <div className="w-full md:w-1/3">RIGHT</div>
