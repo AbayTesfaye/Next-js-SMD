55a270e434456a192ae803db5b417d3c70dc76d5
Abay Tesfaye <abaytesfaye02@gmail.com>
2024-12-15 11:12:45 +0300

    add the calendar,events and announcement for student page

--- src/app/dashboard/student/page.tsx
+++ src/app/dashboard/student/page.tsx
@@ -1,6 +1,17 @@
+import Announcement from "@/components/Announcement"
+import EventCalendar from "@/components/EventCalendar"
+
 const StudentPage = () => {
   return (
-    <div className=''>StudentPage</div>
+    <div className='p-4 flex flex-col gap-4 xl:flex-row'>
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">Left</div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8 ">
+       <EventCalendar/>
+       <Announcement/>
+       </div>
+    </div>
   )
 }
 
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,3 +1,4 @@
+import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -37,12 +38,17 @@ const Menu = () => {
       {menuItems.map((menu) => (
         <div className="flex flex-col gap-2" key={menu.title}>
           <span className="hidden lg:block text-gray-400 font-light my-4">{menu.title}</span>
-            {menu.items.map((menuItem) => (
-              <Link href={menuItem.href} key={menuItem.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
-                <Image src={menuItem.icon} alt={menuItem.label} width={20} height={20} />
-                <span className="hidden lg:block">{menuItem.label}</span>
-              </Link>
-            ))}
+            {menu.items.map((menuItem) => {
+              if(menuItem.visible.includes(role)) {
+                return (
+                  <Link href={menuItem.href} key={menuItem.label}
+                   className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 p-2 rounded-md hover:bg-schoolSkyLight">
+                    <Image src={menuItem.icon} alt={menuItem.label} width={20} height={20} />
+                    <span className="hidden lg:block">{menuItem.label}</span>
+                  </Link>
+                )
+              }
+            })}
         </div>
       ))}
     </div>
