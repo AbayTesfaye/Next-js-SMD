058bf948972e58a36c4ff5b20680b4a34de9f42f
Abay Tesfaye <abaytesfaye02@gmail.com>
2024-12-17 14:10:58 +0300

    Done for result list page

---
+++ src/app/(dashboard)/list/results/page.tsx
@@ -0,0 +1,116 @@
+import Pagination from "@/components/Pagination"
+import Table from "@/components/Table"
+import TableSearch from "@/components/TableSearch"
+import {resultsData,role} from "@/lib/data"
+import Image from "next/image"
+import Link from "next/link"
+
+
+type Result = {
+  id:number;
+  subject:string;
+  student:string;
+  score:number;
+  teacher:string;
+  class:string;
+  date:string;
+}
+
+const columns = [
+    {
+        header: "Subject", 
+        accessor: "name"
+    },
+    {
+        header: "Student", 
+        accessor: "student", 
+        className: "hidden md:table-cell",
+    },
+    {
+        header: "Score", 
+        accessor: "score", 
+        className: "hidden md:table-cell",
+    },
+    {
+        header: "Teacher", 
+        accessor: "teacher", 
+        className: "hidden md:table-cell" 
+    },
+    {
+        header: "Class", 
+        accessor: "class", 
+        className: "hidden md:table-cell",
+    },
+    {
+        header: "Date", 
+        accessor: "date", 
+        className: "hidden md:table-cell"
+    },
+    {
+        header: "Actions", 
+        accessor: "actions", 
+        className: "hidden md:table-cell"
+    },
+]
+
+const ResultListPage = () => {
+    const renderRow = (item: Result) => (
+        <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 hover:bg-schoolPurpleLight">
+          <td className="flex items-center gap-4 p-4">
+            <div>
+              <h1 className="font-medium text-gray-800">{item.subject}</h1>
+            </div>
+          </td>
+          <td className="hidden md:table-cell">{item.student}</td>
+          <td className="hidden md:table-cell">{item.score}</td>
+          <td className="hidden lg:table-cell">{item.teacher}</td>
+          <td className="hidden md:table-cell">{item.class}</td>
+          <td className="hidden lg:table-cell">{item.date}</td>
+          <td>
+            <div className="flex items-center gap-2">
+              <Link href={`/list/students/${item.id}`}>
+               <button className="w-7 h-7 flex items-center justify-center rounded-full bg-schoolSky">
+                <Image src='/edit.png' alt="" width={16} height={16}/>
+               </button>
+              </Link>
+              {
+                role === 'admin' && (
+                  <button className="w-7 h-7 flex items-center justify-center rounded-full bg-schoolPurple">
+                  <Image src='/delete.png' alt="" width={16} height={16}/>
+                 </button>
+                )
+              }
+            </div>
+          </td>
+        </tr>
+      );
+  return (
+    <div className='bg-white rounded-md p-4 flex-1 m-4 mt-0'>
+        {/* TOP */}
+        <div className="flex justify-between items-center">
+            <h1 className="hidden md:block text-lg font-semibold">All Results</h1>
+            <div className="flex flex-col md:flex-row justify-between items-center gap-4 w-full md:w-auto">
+              <TableSearch/>
+              <div className="flex gap-4 self-end items-center ">
+                <button className="bg-schoolYellow rounded-full w-8 h-8 flex items-center justify-center cursor-pointer">
+                    <Image src='/filter.png' width={14} height={14} alt="message"/>
+                </button>
+                <button className="bg-schoolYellow rounded-full w-8 h-8 flex items-center justify-center cursor-pointer">
+                   <Image src='/sort.png' width={14} height={14} alt="message"/>
+                </button>
+                {role === 'admin' && <button className="bg-schoolYellow rounded-full w-8 h-8 flex items-center justify-center cursor-pointer">
+                   <Image src='/plus.png' width={14} height={14} alt="message"/>
+                </button>
+                }  
+              </div>
+            </div>
+        </div>
+        {/* LIST */}
+        <Table columns={columns} renderRow={renderRow} data={resultsData}/>
+        {/* PAGINATION */}
+       <Pagination/>
+    </div>
+  )
+}
+
+export default ResultListPage
