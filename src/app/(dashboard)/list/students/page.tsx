6831b70b6342fbfaf9ade35a7151f0309e0d2096
Abay Tesfaye <abaytesfaye02@gmail.com>
2024-12-17 09:19:43 +0300

    Done for the student list page

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -1,7 +1,7 @@
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
-import { role, teachersData } from "@/lib/data"
+import { role, studentsData} from "@/lib/data"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -11,18 +11,13 @@ const columns = [
         accessor: "info"
     },
     {
-        header: "Teacher ID", 
-        accessor: "teacherid", 
+        header: "Student ID", 
+        accessor: "studentid", 
         className: "hidden md:table-cell"
     },
     {
-        header: "Subjects", 
-        accessor: "subjects", 
-        className: "hidden md:table-cell"
-    },
-    {
-        header: "Classes", 
-        accessor: "classes", 
+        header: "Grade", 
+        accessor: "grade", 
         className: "hidden md:table-cell"
     },
     {
@@ -42,8 +37,8 @@ const columns = [
     },
 ]
 
-const TeachersListPage = () => {
-    const renderRow = (item: Teacher) => (
+const StudentListPage = () => {
+    const renderRow = (item: Student) => (
         <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 hover:bg-schoolPurpleLight">
           <td className="flex items-center gap-4 p-4">
             <Image
@@ -55,17 +50,16 @@ const TeachersListPage = () => {
             />
             <div>
               <h1 className="font-medium text-gray-800">{item.name}</h1>
-              <p className="text-sm text-gray-500">{item.email}</p>
+              <p className="text-sm text-gray-500">{item?.class}</p>
             </div>
           </td>
-          <td className="hidden md:table-cell">{item.teacherId}</td>
-          <td className="hidden md:table-cell">{item.subjects?.join(", ")}</td>
-          <td className="hidden md:table-cell">{item.classes?.join(", ")}</td>
+          <td className="hidden md:table-cell">{item.studentId}</td>
+          <td className="hidden md:table-cell">{item.grade}</td>
           <td className="hidden lg:table-cell">{item.phone}</td>
           <td className="hidden lg:table-cell">{item.address}</td>
           <td>
             <div className="flex items-center gap-2">
-              <Link href={`/list/teachers/${item.id}`}>
+              <Link href={`/list/students/${item.id}`}>
                <button className="w-7 h-7 flex items-center justify-center rounded-full bg-schoolSky">
                 <Image src='/view.png' alt="" width={16} height={16}/>
                </button>
@@ -85,7 +79,7 @@ const TeachersListPage = () => {
     <div className='bg-white rounded-md p-4 flex-1 m-4 mt-0'>
         {/* TOP */}
         <div className="flex justify-between items-center">
-            <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+            <h1 className="hidden md:block text-lg font-semibold">All Students</h1>
             <div className="flex flex-col md:flex-row justify-between items-center gap-4 w-full md:w-auto">
               <TableSearch/>
               <div className="flex gap-4 self-end items-center ">
@@ -95,20 +89,19 @@ const TeachersListPage = () => {
                 <button className="bg-schoolYellow rounded-full w-8 h-8 flex items-center justify-center cursor-pointer">
                    <Image src='/sort.png' width={14} height={14} alt="message"/>
                 </button>
-                 {role === 'admin' && 
-                   <button className="bg-schoolYellow rounded-full w-8 h-8 flex items-center justify-center cursor-pointer">
+                {role === 'admin' && <button className="bg-schoolYellow rounded-full w-8 h-8 flex items-center justify-center cursor-pointer">
                    <Image src='/plus.png' width={14} height={14} alt="message"/>
-                   </button>
-                  } 
+                </button>
+                }  
               </div>
             </div>
         </div>
         {/* LIST */}
-        <Table columns={columns} renderRow={renderRow} data={teachersData}/>
+        <Table columns={columns} renderRow={renderRow} data={studentsData}/>
         {/* PAGINATION */}
        <Pagination/>
     </div>
   )
 }
 
-export default TeachersListPage
+export default StudentListPage
