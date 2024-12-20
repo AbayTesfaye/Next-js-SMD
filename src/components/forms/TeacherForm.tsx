cc5f5dcced158b62ad530a2f3ef0e2c901d27579
Abay Tesfaye <abaytesfaye02@gmail.com>
2024-12-20 17:47:40 +0300

    Update some UI

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -32,8 +32,11 @@ const TeacherForm = ({type,data}:{type:'create' | 'update'; data?:any;}) => {
     <form className='flex flex-col gap-8' onSubmit={onSubmit}>
        <h1 className='text-xl font-semibold'>Create a new teacher</h1>
        <span className='text-xs text-gray-400 font-medium'>Authentication Information</span>
+       <div className='flex flex-col gap-2 w-full md:w-1/4'>
+       <label htmlFor="" className='text-xs text-gray-500'>Username</label>
        <input type="text" {...register('username')} className='ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm' />
-       {errors.username?.message && <p>{errors.username?.message.toString()}</p>}
+       {errors.username?.message && <p className='text-red-400 text-xs'>{errors.username?.message.toString()} </p>}
+       </div>
        <span className='text-xs text-gray-400 font-medium'>Personal Information</span>
        <button className='bg-blue-400 text-white p-2 rounded-md'>
         {type === 'create' ? "Create" :"Update"}
